5b912b5bc4b5d95da420878cb6d776d836a508aa
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-19 13:15:37 +0000

    update datasource user

--- product-service/src/main/resources/scripts.db_init.sql
+++ product-service/src/main/resources/scripts.db_init.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS productservice;
-DROP USER IF EXISTS `product_service`@`%`;
+DROP USER IF EXISTS `warehouse_user`@`%`;
 CREATE DATABASE IF NOT EXISTS productservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `product_service`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON `productservice`.* TO `product_service`@`%`;
+CREATE USER IF NOT EXISTS `warehouse_user`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON `productservice`.* TO `warehouse_user`@`%`;
