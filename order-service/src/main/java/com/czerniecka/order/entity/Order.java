38821683ea9dd8e15565525c624b006593fb28d6
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-19 13:26:17 +0000

    basic order layers

--- order-service/src/main/java/com/czerniecka/order/entity/Order.java
+++ order-service/src/main/java/com/czerniecka/order/entity/Order.java
@@ -1,4 +1,31 @@
 package com.czerniecka.order.entity;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+    private String paymentType;
+    private String orderStatus;
+    private LocalDateTime orderPlaced;
+
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID productId;
+
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID customerId;
+
 }
