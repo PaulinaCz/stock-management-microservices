4994cd062f83f93c7f5945859b0198645a0e8240
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-19 14:13:41 +0000

    basic supplier layers

---
+++ supplier-service/src/main/java/com/czerniecka/supplier/controller/SupplierController.java
@@ -0,0 +1,9 @@
+package com.czerniecka.supplier.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/suppliers")
+public class SupplierController {
+}
---
+++ supplier-service/src/main/java/com/czerniecka/supplier/entity/Supplier.java
@@ -0,0 +1,9 @@
+package com.czerniecka.supplier.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Supplier {
+
+
+}
