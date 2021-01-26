cc1ee234be44ee04a00e909f8e2c9bc6b96ff9ad
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-26 20:11:46 +0000

    adds eureka-client properties

--- supplier-service/src/main/java/com/czerniecka/supplier/SupplierServiceApplication.java
+++ supplier-service/src/main/java/com/czerniecka/supplier/SupplierServiceApplication.java
@@ -2,8 +2,10 @@ package com.czerniecka.supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SupplierServiceApplication {
 
     public static void main(String[] args) {
