5b7843cc9f82e050426e055afd0c2babf0541970
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-29 18:16:35 +0000

    order testing

--- order-service/src/test/java/com/czerniecka/order/service/OrderServiceTest.java
+++ order-service/src/test/java/com/czerniecka/order/service/OrderServiceTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 import static org.mockito.Mockito.when;
 
 
