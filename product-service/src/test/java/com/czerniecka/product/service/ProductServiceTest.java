c11eebd7e37becd27f534196d7e21ea857a81774
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-29 17:52:16 +0000

    product testing

--- product-service/src/test/java/com/czerniecka/product/service/ProductServiceTest.java
+++ product-service/src/test/java/com/czerniecka/product/service/ProductServiceTest.java
@@ -4,11 +4,15 @@ import com.czerniecka.product.dto.ProductDTO;
 import com.czerniecka.product.dto.ProductMapper;
 import com.czerniecka.product.entity.Product;
 import com.czerniecka.product.repository.ProductRepository;
+import com.czerniecka.product.vo.Inventory;
+import com.czerniecka.product.vo.InventoryRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +23,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,8 +32,8 @@ public class ProductServiceTest {
 
     @Mock
     ProductRepository productRepository;
-    @Mock
-    ProductMapper productMapper;
+    @Spy
+    ProductMapper productMapper = Mappers.getMapper(ProductMapper.class);
     @Mock
     RestTemplate restTemplate;
 
@@ -46,7 +51,20 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void shouldReturnAll() {
+    public void shouldMapProductToProductDTO(){
+
+        Product product = new Product(UUID.randomUUID(), "Product", BigDecimal.ONE, BigDecimal.TEN,
+                "Category", LocalDateTime.now(), LocalDateTime.now(), UUID.randomUUID());
+
+        ProductDTO productDTO = productMapper.toProductDTO(product);
+
+        assertThat(productDTO.getName().equals("Product"));
+        assertThat(productDTO.getCategory().equals("Category"));
+
+    }
+
+    @Test
+    public void shouldReturnAllProducts() {
 
         List<Product> products = new ArrayList<>();
         Product product = new Product(UUID.randomUUID(), "Product", BigDecimal.ONE, BigDecimal.TEN,
@@ -66,10 +84,24 @@ public class ProductServiceTest {
     @Test
     public void shouldReturnOptionalEmpty(){
 
-        when(productRepository.findAll()).thenReturn(List.of());
-
-        Optional<ProductDTO> productById = productService.findProductById(UUID.randomUUID());
+Optional<ProductDTO> productById = productService.findProductById(UUID.randomUUID());
         Assertions.assertEquals(Optional.empty(), productById);
     }
 
+    @Test
+    public void shouldReturnSavedObject(){
+
+        Product product = new Product(UUID.randomUUID(), "Product", BigDecimal.ONE, BigDecimal.TEN,
+                "Category", LocalDateTime.now(), LocalDateTime.now(), UUID.randomUUID());
+
+        ProductDTO productDTO = new ProductDTO();
+
+        when(productRepository.save(productMapper.toProduct(productDTO))).thenReturn(product);
+        ProductDTO saved = productService.save(new InventoryRequest(new Inventory(), productDTO));
+
+        assertThat(saved).isNotNull();
+        assertThat(saved.getCategory()).isEqualTo("Category");
+        assertThat(saved.getName()).isEqualTo("Product");
+
+    }
 }
