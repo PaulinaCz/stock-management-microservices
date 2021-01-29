eaf0177f0158fdc1d568ffb7861644fdb6c576d0
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-29 17:20:09 +0000

    product testing

--- product-service/src/test/java/com/czerniecka/product/service/ProductServiceTest.java
+++ product-service/src/test/java/com/czerniecka/product/service/ProductServiceTest.java
@@ -7,25 +7,21 @@ import com.czerniecka.product.repository.ProductRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
 
@@ -33,13 +29,14 @@ public class ProductServiceTest {
     ProductRepository productRepository;
     @Mock
     ProductMapper productMapper;
+    @Mock
+    RestTemplate restTemplate;
 
-    @InjectMocks
     ProductService productService;
 
     @Before
     public void setUp() {
-        when(productMapper.toProductDTO(any(Product.class))).thenReturn(new ProductDTO());
+        productService = new ProductService(productRepository, productMapper, restTemplate);
     }
 
 
@@ -62,9 +59,17 @@ public class ProductServiceTest {
         when(productRepository.findAll()).thenReturn(products);
 
         List<ProductDTO> all = productService.findAll();
+        Assertions.assertEquals(2, all.size());
 
-        Assertions.assertEquals(3, all.size());
+    }
 
+    @Test
+    public void shouldReturnOptionalEmpty(){
+
+        when(productRepository.findAll()).thenReturn(List.of());
+
+        Optional<ProductDTO> productById = productService.findProductById(UUID.randomUUID());
+        Assertions.assertEquals(Optional.empty(), productById);
     }
 
 }
