a33b5088e92d46ab457cf37309ecb27faf7cfc68
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-09 11:36:21 +0000

    adds input validation on save order

--- order-service/src/main/java/com/czerniecka/order/controller/OrderController.java
+++ order-service/src/main/java/com/czerniecka/order/controller/OrderController.java
@@ -4,13 +4,16 @@ import com.czerniecka.order.dto.OrderDTO;
 import com.czerniecka.order.service.OrderService;
 import com.czerniecka.order.vo.OrderProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
@@ -46,10 +49,28 @@ public class OrderController {
     }
 
     @PostMapping("")
-    public ResponseEntity<OrderDTO> addOrder(@RequestBody OrderDTO orderDTO){
+    public ResponseEntity<OrderDTO> addOrder(@Valid @RequestBody OrderDTO orderDTO){
         Optional<OrderDTO> saved = orderService.save(orderDTO);
         return saved.map(order -> new ResponseEntity<>(order, HttpStatus.CREATED))
                 .orElseGet(() -> new ResponseEntity("Service is currently busy. Please try again later.",
                         HttpStatus.SERVICE_UNAVAILABLE));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+
+        Map<String, Object> errorBody = new HashMap<>();
+
+        errorBody.put("timestamp", LocalDateTime.now());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        errorBody.put("validationErrors", errors);
+        return errorBody;
     }
 }
