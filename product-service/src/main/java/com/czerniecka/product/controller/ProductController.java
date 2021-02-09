026d48648fa8ab954580787ff5bbf0f485f789f7
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-09 10:36:39 +0000

    adds input validation on save product

--- product-service/src/main/java/com/czerniecka/product/controller/ProductController.java
+++ product-service/src/main/java/com/czerniecka/product/controller/ProductController.java
@@ -4,13 +4,16 @@ import com.czerniecka.product.dto.ProductDTO;
 import com.czerniecka.product.service.ProductService;
 import com.czerniecka.product.vo.ProductSupplierResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
@@ -59,11 +62,29 @@ public class ProductController {
     }
 
     @PostMapping("")
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO) {
         Optional<ProductDTO> saved = productService.save(productDTO);
         return saved.map(product -> new ResponseEntity<>(product, HttpStatus.CREATED))
                 .orElseGet(() -> new ResponseEntity("Error while creating product inventory. Product not saved.",
                         HttpStatus.SERVICE_UNAVAILABLE));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+
+        Map<String, Object> errorBody = new HashMap<>();
+
+        errorBody.put("timestamp", LocalDateTime.now());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        errorBody.put("validationErrors", errors);
+        return errorBody;
     }
 
 }
--- product-service/src/main/java/com/czerniecka/product/dto/ProductDTO.java
+++ product-service/src/main/java/com/czerniecka/product/dto/ProductDTO.java
@@ -2,15 +2,25 @@ package com.czerniecka.product.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 public class ProductDTO {
-    
+
+    @NotBlank(message = "Please provide product name")
     private String name;
+    @Positive
     private BigDecimal buyingPrice;
+    @Positive
     private BigDecimal sellingPrice;
+    @NotBlank(message = "Please provide category")
+    @Size(max = 20, message = "Category cannot be longer than 20 characters")
     private String category;
+    @NotNull(message = "Please add supplier")
     private UUID supplierId;
 }
