cc51846c9667154483e8aa3038e18919bb7c8eae
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-09 12:01:18 +0000

    add input validation on save invoice

--- invoice-service/src/main/java/com/czerniecka/invoice/controller/InvoiceController.java
+++ invoice-service/src/main/java/com/czerniecka/invoice/controller/InvoiceController.java
@@ -4,13 +4,16 @@ import com.czerniecka.invoice.dto.InvoiceDTO;
 import com.czerniecka.invoice.service.InvoiceService;
 import com.czerniecka.invoice.vo.InvoiceProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/invoices")
@@ -38,11 +41,29 @@ public class InvoiceController {
     }
 
     @PostMapping("")
-    public ResponseEntity<InvoiceDTO> addInvoice(@RequestBody InvoiceDTO invoiceDTO){
+    public ResponseEntity<InvoiceDTO> addInvoice(@RequestBody @Valid InvoiceDTO invoiceDTO){
         Optional<InvoiceDTO> saved = invoiceService.save(invoiceDTO);
         return saved.map(invoice -> new ResponseEntity<>(invoice, HttpStatus.CREATED))
                 .orElseGet(() -> new ResponseEntity("Error while processing invoice, please try again later.",
                         HttpStatus.SERVICE_UNAVAILABLE));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+
+        Map<String, Object> errorBody = new HashMap<>();
+
+        errorBody.put("timestamp", LocalDateTime.now());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        errorBody.put("validationErrors", errors);
+        return errorBody;
     }
 
 }
