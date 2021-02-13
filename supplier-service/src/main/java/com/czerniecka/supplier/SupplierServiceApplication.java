5b96f88914361eb228b02921b2b01fdc6dbb2fd8
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-13 12:32:08 +0000

    init webflux and mongodb connection

--- supplier-service/src/main/java/com/czerniecka/supplier/SupplierServiceApplication.java
+++ supplier-service/src/main/java/com/czerniecka/supplier/SupplierServiceApplication.java
@@ -3,9 +3,11 @@ package com.czerniecka.supplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableReactiveMongoRepositories
 public class SupplierServiceApplication {
 
     public static void main(String[] args) {
