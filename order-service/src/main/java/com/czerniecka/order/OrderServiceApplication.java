2877a392b9176454c020b1721e8491984b893729
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-13 14:47:09 +0000

    init webflux and mongodb connection

--- order-service/src/main/java/com/czerniecka/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/czerniecka/order/OrderServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableReactiveMongoRepositories
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
