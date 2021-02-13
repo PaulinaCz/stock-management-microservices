861816d12abc5240a301ab028d33bfe5188e7a50
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-13 20:09:17 +0000

    refactoring

--- product-service/src/main/java/com/czerniecka/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/czerniecka/product/ProductServiceApplication.java
@@ -1,14 +1,11 @@
 package com.czerniecka.product;
 
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -20,12 +17,7 @@ public class ProductServiceApplication {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
-
+    //@LoadBalanced - or should be separate?
     @Bean
     public WebClient.Builder getWebClientBuilder(){
         return WebClient.builder();
--- product-service/src/main/resources/scripts/db_init.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS productservice;
-DROP USER IF EXISTS `warehouse_user`@`%`;
-CREATE DATABASE IF NOT EXISTS productservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `warehouse_user`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON `productservice`.* TO `warehouse_user`@`%`;
