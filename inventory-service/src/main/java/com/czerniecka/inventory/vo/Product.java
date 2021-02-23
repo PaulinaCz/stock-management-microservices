5fa045c235c5284a03b45960feb9d70c8d0bd5ae
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-23 18:38:09 +0000

    adds productID to response

--- inventory-service/src/main/java/com/czerniecka/inventory/vo/Product.java
+++ inventory-service/src/main/java/com/czerniecka/inventory/vo/Product.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Product {
 
+    private String id;
     private String name;
     private BigDecimal sellingPrice;
     private BigDecimal buyingPrice;
