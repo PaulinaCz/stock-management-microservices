def39f3c909a1aeac63bd929245ebd25853e6337
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-25 15:25:40 +0000

    adds blocking way to retrieve object from supplier-service

--- product-service/src/main/java/com/czerniecka/product/client/SupplierServiceClient.java
+++ product-service/src/main/java/com/czerniecka/product/client/SupplierServiceClient.java
@@ -1,16 +1,10 @@
 package com.czerniecka.product.client;
 
-import com.czerniecka.product.dto.ProductDTO;
-import com.czerniecka.product.exceptions.ProductNotFound;
-import com.czerniecka.product.vo.ProductSupplierResponse;
 import com.czerniecka.product.vo.Supplier;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import javax.naming.ServiceUnavailableException;
 
 @Service
 public class SupplierServiceClient {
@@ -23,34 +17,12 @@ public class SupplierServiceClient {
         this.webClientBuilder = webClientBuilder;
     }
 
+    /**
+     * For project simplicity GET supplier
+     * will be subscribed in a  blocking way
+     * */
+
 //    @CircuitBreaker(name="supplier-service", fallbackMethod = "fallback")
-//    public Mono<ProductSupplierResponse> getProductWithSupplier(String productId){
-//
-//        return webClientBuilder.build()
-//                .get()
-//                .uri("http://product-service/products/" + productId)
-//                .retrieve()
-//                .bodyToMono(ProductDTO.class)
-//                .switchIfEmpty(Mono.error(new ProductNotFound(productId)))
-//                .flatMap(
-//                        p -> {
-//                              Mono<Supplier> s = webClientBuilder.build()
-//                                      .get()
-//                                      .uri("http://supplier-service/suppliers/")
-//                                    .retrieve()
-//                                    .bodyToMono(Supplier.class)
-//                                    .onErrorResume(e -> Mono.error(
-//                                            new ServiceUnavailableException(
-//                                                    "Service is currently busy. Please try again later")));
-//                              return Mono.just(new ProductSupplierResponse(p, s));
-//                        }
-//                );
-//
-//
-//    }
-
-
-    @CircuitBreaker(name="supplier-service", fallbackMethod = "fallback")
     public Supplier getSupplier(String supplierId){
 
         return webClientBuilder.build()
@@ -58,16 +30,12 @@ public class SupplierServiceClient {
                 .uri("http://supplier-service/suppliers/" + supplierId)
                 .retrieve()
                 .bodyToMono(Supplier.class)
-                .onErrorResume(e -> Mono.error(
-                        new ServiceUnavailableException(
-                                "Service is currently busy. Please try again later")
-                ))
+                .onErrorReturn(new Supplier())
                 .block();
-
     }
-
-    public Supplier fallback(String supplierId, Throwable throwable){
-        return new Supplier();
-    }
+//    public Supplier fallback(String supplierId, Throwable throwable){
+//        System.out.println("Service is currently busy. Please try again later.");
+//        return new Supplier();
+//    }
 
 }
