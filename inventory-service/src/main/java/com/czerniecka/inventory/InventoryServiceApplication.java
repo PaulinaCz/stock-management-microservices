20dbf1c7eae8771b828c2719b1bdf87b9b6acff0
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-28 14:47:08 +0000

    fixes r4j circuitbreaker

--- inventory-service/src/main/java/com/czerniecka/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/czerniecka/inventory/InventoryServiceApplication.java
@@ -1,12 +1,20 @@
 package com.czerniecka.inventory;
 
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+import static io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.ofDefaults;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,6 +25,13 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build()).build());
+    }
+    
     @Bean
     @LoadBalanced
     public WebClient.Builder getWebClientBuilder(){
--- inventory-service/src/main/java/com/czerniecka/inventory/client/ProductServiceClient.java
+++ inventory-service/src/main/java/com/czerniecka/inventory/client/ProductServiceClient.java
@@ -3,38 +3,34 @@ package com.czerniecka.inventory.client;
 import com.czerniecka.inventory.dto.InventoryDTO;
 import com.czerniecka.inventory.vo.InventoryProductResponse;
 import com.czerniecka.inventory.vo.Product;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import javax.naming.ServiceUnavailableException;
-
 
 @Service
 public class ProductServiceClient {
 
     private WebClient.Builder webClientBuilder;
+    final ReactiveCircuitBreaker rcb;
 
     @Autowired
-    public ProductServiceClient(WebClient.Builder webClientBuilder) {
+    public ProductServiceClient(WebClient.Builder webClientBuilder, ReactiveCircuitBreakerFactory cbFactory) {
         this.webClientBuilder = webClientBuilder;
+        this.rcb = cbFactory.create("product-service-cb");
     }
 
-    @CircuitBreaker(name= "product-service-cb", fallbackMethod = "productFallback")
     public Mono<InventoryProductResponse> getProduct(String  productId, InventoryDTO inventory){
-        
-        return webClientBuilder.build()
+
+        Mono<InventoryProductResponse> response = webClientBuilder.build()
                 .get()
                 .uri("http://product-service/products/" + productId)
                 .retrieve()
                 .bodyToMono(Product.class)
-                .onErrorReturn(new Product())
                 .flatMap(product -> Mono.just(new InventoryProductResponse(inventory, product)));
-    }
 
-    public Mono<InventoryProductResponse> productFallback(String  productId, InventoryDTO inventory, Throwable throwable){
-        return Mono.error(new ServiceUnavailableException("Service is currently busy. Please try again later."));
+        return rcb.run(response, throwable -> Mono.just(new InventoryProductResponse(inventory, new Product())));
     }
 }
