b399978a1e105712e9fe76772de5a7384097dc5a
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-28 14:26:08 +0000

    fixes r4j circuitbreaker

--- product-service/src/main/java/com/czerniecka/product/client/SupplierServiceClient.java
+++ product-service/src/main/java/com/czerniecka/product/client/SupplierServiceClient.java
@@ -4,35 +4,34 @@ import com.czerniecka.product.dto.ProductDTO;
 import com.czerniecka.product.vo.ProductSupplierResponse;
 import com.czerniecka.product.vo.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+
 @Service
 public class SupplierServiceClient {
 
     private WebClient.Builder webClientBuilder;
-
-    private final ReactiveCircuitBreakerFactory cbFactory;
+    final ReactiveCircuitBreaker rcb;
 
     @Autowired
     public SupplierServiceClient(WebClient.Builder webClientBuilder, ReactiveCircuitBreakerFactory cbFactory) {
         this.webClientBuilder = webClientBuilder;
-        this.cbFactory = cbFactory;
+        this.rcb = cbFactory.create("supplier-service-cb");
     }
 
     public Mono<ProductSupplierResponse> getSupplier(String supplierId, ProductDTO product){
 
-        return webClientBuilder.build()
+        Mono<ProductSupplierResponse> response = webClientBuilder.build()
                 .get()
                 .uri("http://supplier-service/suppliers/" + supplierId)
                 .retrieve()
                 .bodyToMono(Supplier.class)
-                .flatMap(supplier -> Mono.just(new ProductSupplierResponse(product, supplier)))
-                .transform(
-                        it -> cbFactory.create("supplier-service-cb")
-                                .run(it, throwable -> Mono.just(new ProductSupplierResponse(product, new Supplier())))
-                );
+                .flatMap(supplier -> Mono.just(new ProductSupplierResponse(product, supplier)));
+
+        return rcb.run(response, throwable -> Mono.just(new ProductSupplierResponse(product, new Supplier())));
     }
 }
