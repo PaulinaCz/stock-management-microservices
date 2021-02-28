35ac6c3a3619ad04e811e64205f6b5b8791af96b
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-02-28 14:31:46 +0000

    fixes r4j circuitbreaker

--- product-service/src/main/java/com/czerniecka/product/client/InventoryServiceClient.java
+++ product-service/src/main/java/com/czerniecka/product/client/InventoryServiceClient.java
@@ -2,6 +2,7 @@ package com.czerniecka.product.client;
 
 import com.czerniecka.product.vo.Inventory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,28 +14,25 @@ import javax.naming.ServiceUnavailableException;
 public class InventoryServiceClient {
 
     private WebClient.Builder webClientBuilder;
-    private ReactiveCircuitBreakerFactory cbFactory;
+    final ReactiveCircuitBreaker rcb;
 
     @Autowired
     public InventoryServiceClient(WebClient.Builder webClientBuilder, ReactiveCircuitBreakerFactory cbFactory) {
         this.webClientBuilder = webClientBuilder;
-        this.cbFactory = cbFactory;
+        this.rcb = cbFactory.create("inventory-service-cb");
     }
 
     public Mono<Inventory> postInventory(Inventory inventory){
 
-        return webClientBuilder.build()
+        Mono<Inventory> i = webClientBuilder.build()
                 .post()
                 .uri("http://inventory-service/inventories")
                 .body(Mono.just(inventory), Inventory.class)
                 .retrieve()
-                .bodyToMono(Inventory.class)
-                .transform(
-                        it -> cbFactory.create("inventory-service-cb")
-                        .run(it, throwable -> Mono.error(
-                                new ServiceUnavailableException("Error while creating product inventory. Product not saved.")
-                        ))
-                );
+                .bodyToMono(Inventory.class);
+
+        return rcb.run(i, throwable -> Mono.error(
+                               new ServiceUnavailableException("Error while creating product inventory. Product not saved.")));
 
     }
 }
